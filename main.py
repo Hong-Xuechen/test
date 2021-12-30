84f86905a191c162b42946db3eaa29f8d2404cdc
洪学辰 <73264768+Hong-Xuechen@users.noreply.github.com>
2021-12-30 17:52:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ class Item(BaseModel):
     text:str
         
 app = FastAPI()
-classifier = pipeline("question-answering")
+nlp = pipeline("question-answering")
+
+context = r"""
+Extractive Question Answering is the task of extracting an answer from a text given a question. An example of a
+question answering dataset is the SQuAD dataset, which is entirely based on that task. If you would like to fine-tune
+a model on a SQuAD task, you may leverage the examples/question-answering/run_squad.py script.
+"""
+
+result = nlp(question=item.text, context=context)
 
 @app.get("/")
 def root():
     return {"Hello": "World"}
 
-@app.put("/translator")
-def translator(item: Item):
-    return {"translator": classifier(item.text)}
+@app.put("/question-answering")
+def question-answering(item: Item):
+    return {"question-answering": result}
