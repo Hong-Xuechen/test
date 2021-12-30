f5c8fcfb9080f9ce2c48e76cd7a0cdb68ff68764
洪学辰 <73264768+Hong-Xuechen@users.noreply.github.com>
2021-12-30 16:52:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Item(BaseModel):
     text:str
         
 app = FastAPI()
-classifier = pipeline("summarization","sshleifer/distilbart-xsum-1-1")
+classifier = pipeline("fill-mask","ckiplab/albert-tiny-chinese")
 
 @app.get("/")
 def root():
