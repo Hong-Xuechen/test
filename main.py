60f4dbd5407d965aa88c8968d9c1062d828e4c79
洪学辰 <73264768+Hong-Xuechen@users.noreply.github.com>
2021-12-30 13:46:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from transformers import AutoModelWithLMHead,AutoTokenizer,pipeline
-mode_name = 'liam168/trans-opus-mt-zh-en'
-model = AutoModelWithLMHead.from_pretrained(mode_name)
-tokenizer = AutoTokenizer.from_pretrained(mode_name)
-translation = pipeline("translation_zh_to_en", model=model, tokenizer=tokenizer)
-translation('我喜欢学习数据科学和机器学习。', max_length=400)
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
+
+class Item(BaseModel):
+    text:str
+        
+app = FastAPI()
+translator_en_to_zh = pipeline( "text-classification","bhadresh-savani/distilbert-base-uncased-emotion")
+
+@app.get("/")
+def root():
+    return {"Hello": "World"}
+
+@app.put("/translator/")
+def translator(item: Item):
+    return {"translator": translator_en_to_zh(item.text)}
